db0482f324ba6b750710ed69682b7bb5fc655bc4
Tyuiu.ChalkovaEM@outlook.com <tch4cat@yandex.ru>
2023-10-25 18:05:44 +0500

    Task6

---
+++ Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Test/DataServceTest.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Lib;
+
+namespace Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Test
+{
+    [TestClass]
+    public class DataServceTest
+    {
+        [TestMethod]
+        public void ValidGetSumTheDivisors()
+        {
+            DataService ds = new DataService();
+            int startValue = 5;
+            int stopValue = 16;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 42;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ChalkovaE.M.Sprint3.Task6.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f37dd512-4a53-4940-a1d6-4a1b0c3f52ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
